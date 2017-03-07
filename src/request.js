fdd36909701c0518cd29e54f1354779d9ac53ea3
Andrii Peliak <vseprav@gmail.com>
2017-03-07 17:25:54 +0200

    implement create ticket method

--- src/request.js
+++ src/request.js
@@ -1,13 +1,24 @@
 let request = {
   get:function(uri, auth_tocken) {
     return fetch(uri, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': auth_tocken
-          }
-        })
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': auth_tocken
+      }
+    })
+  },
+  post:function (uri, auth_tocken, data) {
+    return fetch(uri, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': auth_tocken
+      },
+      body: JSON.stringify(data)
+    })
   }
 
-}
+};
+
 module.exports = request;
--- src/tickets.js
+++ src/tickets.js
@@ -5,6 +5,9 @@ let Tickets = function(config, entity){
   return {
     list:function() {
       return request.get(config.url  + '/' +  entity.plural + '.json', config.auth_tocken);
+    },
+    create:function (data) {
+      return request.post(config.url  + '/' +  entity.plural + '.json', config.auth_tocken, data);
     }
   };
 };
